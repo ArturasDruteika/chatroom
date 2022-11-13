c7217bc5bafb1d53c1a15dec7e93751b43400955
Arturas <arturas0000@gmail.com>
2022-11-13 21:13:31 +0200

    Cleaned up code a bit, put the whole functionality into a startServer function()

--- src/main.cpp
+++ src/main.cpp
@@ -9,39 +9,26 @@
 
 #define PORT 8080
 
-int main(int argc, char const *argv[])
+
+void startServer()
 {
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
-
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
 
-    // Creating socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    if (server_fd < 0)
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd,
-                   SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT,
-                   &opt,
-                   sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
 
     // Forcefully attaching socket to the port 8080
     if (bind(server_fd,
@@ -66,7 +53,7 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    valread = read(new_socket, buffer, 1024);
+    read(new_socket, buffer, 1024);
     printf("%s\n", buffer);
     send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
@@ -75,6 +62,12 @@ int main(int argc, char const *argv[])
     close(new_socket);
     // closing the listening socket
     shutdown(server_fd, SHUT_RDWR);
+}
+
+
+int main()
+{
+    startServer();
 
     return 0;
 }
